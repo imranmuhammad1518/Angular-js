f884f43852393b308fe082da62c47a5f5ad7b6ee
imranmuhammad1518 <madhusparkyy@gmail.com>
2017-07-17 18:31:43 +0530

    Add files via upload

---
+++ index-controller.js
@@ -0,0 +1,17 @@
+app.controller('IndexController',['$scope', function($scope){
+	
+	//$scope.message = "Hello Angular!";
+	
+	//$scope.postData = function(){
+	//	alert($scope.username);
+	//}
+	
+	$scope languages = [
+	"Alfreds Futterkiste",
+        "Berglunds snabbköp",
+        "Centro comercial Moctezuma",
+        "Ernst Handel",
+	
+	]
+	
+}]);
